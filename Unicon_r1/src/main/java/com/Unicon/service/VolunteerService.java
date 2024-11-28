18415f582b9af44c9cf4baa8208431f097e69972
kimth <kimth@kimthLaptop>
2024-11-28 17:44:14 +0900

    봉사활동 신청현황 취소기능

--- Unicon_r1/src/main/java/com/Unicon/service/VolunteerService.java
+++ Unicon_r1/src/main/java/com/Unicon/service/VolunteerService.java
@@ -24,7 +24,7 @@ public interface VolunteerService {
     void submitApplication(VolunteerApplyVO application) throws Exception;
     void approveApplication(Long voId) throws Exception;
     void rejectApplication(Long voId, String reason) throws Exception;
-    void cancelApplication(Long voId, String reason) throws Exception;
+    void cancelApplication(Long voId, String reason, String reasonDetail) throws Exception;
     List<VolunteerApplyVO> getMyApplications(String userId) throws Exception;
     Map<String, Object> getApplicationList(Long volunteerId, String status) throws Exception;
     List<VolunteerVO> getDraftList() throws Exception;
