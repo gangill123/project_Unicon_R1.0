1ad536dc96187dd98e48d4ea08bd54f657225a20
82107 <82107@183.103.7.223>
2024-12-07 23:57:44 +0900

    상품등록 유효성 남았음

--- Unicon_r1/src/main/java/com/Unicon/persistence/ProductDAO.java
+++ Unicon_r1/src/main/java/com/Unicon/persistence/ProductDAO.java
@@ -1,5 +1,7 @@
 package com.Unicon.persistence;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -8,55 +10,43 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import com.Unicon.domain.ImageVO;
+import com.Unicon.domain.OptionVO;
 import com.Unicon.domain.ProductVO;
-
-
 
 @Repository("productDAO")
 public class ProductDAO {
-	
+
 	@Inject
-	private SqlSession sqlSession; 
+	private SqlSession sqlSession;
 	private static final Logger logger = LoggerFactory.getLogger(ProductDAO.class);
 
 	private static final String NAMESPACE = "com.Unicon.mapper.productMapper.";
-	
+
 	public void productInsert(ProductVO vo) {
 		logger.debug("( •̀ ω •́ )✧ productInsert() 실행");
-		logger.debug("vo : "+ vo);
+		logger.debug("vo : " + vo);
 		
-		sqlSession.insert(NAMESPACE+"insertProduct", vo);
 		
+		// 상품등록 (옵션, 이미지 뺴고)등록
+		sqlSession.insert(NAMESPACE + "insertProduct", vo);
+
 		// 마지막으로 삽입된 product_id 가져오기
-	    String lastInsertedId = (String) sqlSession.selectOne(NAMESPACE + "selectLastInsertedProductId");
-	    vo.setProduct_id(lastInsertedId);
-	    
-		/* sqlSession.insert(NAMESPACE+"isertOption",vo); */
-	    
+		String InsertedProductId = (String) sqlSession.selectOne(NAMESPACE + "selectInsertedProductId", vo);
 		
+		//option을 리스트에 받아온 상품id 담아서 insert
+		List<OptionVO> optionVo = new ArrayList<OptionVO>(vo.getOption());
+		for(OptionVO option : optionVo) {
+			option.setProduct_id(InsertedProductId);
+		}
+		sqlSession.insert(NAMESPACE + "insertOptions", optionVo);
 		
-		
-		
-		/*
-		 * List<ImageVO> images = new ArrayList<ImageVO>(avo.getAnimal_images());
-		 * sqlSession.insert(NAMESPACE+"insertAnimalImages", images);
-		 * 
-		 * Map<String, Object> healthParams = new HashMap<>();
-		 * healthParams.put("animal_id", avo.getAnimal_id());
-		 * healthParams.put("healths", avo.getAnimal_healths());
-		 * sqlSession.insert(NAMESPACE+"insertAnimalHealths", healthParams);
-		 * 
-		 * Map<String, Object> vaccineParams = new HashMap<>();
-		 * vaccineParams.put("animal_id", avo.getAnimal_id());
-		 * vaccineParams.put("vaccines", avo.getAnimal_vaccines());
-		 * sqlSession.insert(NAMESPACE+"insertAnimalVaccines", vaccineParams);
-		 */
+		// 상품 images insert
+		List<ImageVO> images = new ArrayList<ImageVO>(vo.getProduct_images());
+		for(ImageVO image : images) {
+			image.setImage_id(InsertedProductId);
+		}
+		sqlSession.insert(NAMESPACE + "insertProductImages", images);
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
